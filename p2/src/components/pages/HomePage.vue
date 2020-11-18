607f22e2000d4750d368dc43eb7e5f27e895d2d6
MystPhysX <contact@mystphy.sx>
2020-11-18 07:55:11 +0530

    Added an introduction to the site

--- p2/src/components/pages/HomePage.vue
+++ p2/src/components/pages/HomePage.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="posts">
+    <p>A work in progress micro blog site. You can find the last three written posts on this homepage or you can visit the "all" page to see a list of all posts ever. 
+      Clicking on any post will take you to the page for that post where you can comment and read comments from other users. 
+      You can also submit your own post in an anonymous fashion by clicking submit above.</p>
     <h2>Most Recent Micro Blog Posts</h2>
     <router-link
       v-for="post in recentPosts"
